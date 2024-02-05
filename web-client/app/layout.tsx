1a0b542146448973c410813d2987143e72e81f29
Kay Tang <126426690+ktan185@users.noreply.github.com>
2024-02-05 13:51:48 +1300

    style: Updated directory names

--- web-client/app/layout.tsx
+++ web-client/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './navbar/navbar'
-import { UserProfilePicture } from './navbar/user/user'
 import { UserProvider } from './context/UserContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'YouTube',
-  description: 'YouTube Clone',
+  title: 'myKilps',
+  description:
+    'Video processing web application heavily inspired from YouTube to learn GCP and fullstack',
 }
 
 export default function RootLayout({
